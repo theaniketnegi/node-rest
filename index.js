9eb899aeb2254f4436f91ee5eb26604f71c776e3
Aniket <aniketng08@gmail.com>
2023-11-30 17:43:45 +0530

    Added DELETE and PATCH

--- index.js
+++ index.js
@@ -25,19 +25,31 @@ app
     return res.json(users.find((user) => user.id === id));
   })
   .patch((req, res) => {
-    //Edit user with ID
-    return res.json({ status: "Pending" });
+    const id = Number(req.params.id);
+    const newUser = req.body;
+
+    const newUsers = users.map((user) => {
+      if (user.id === id) return { ...newUser, id };
+      else return user;
+    });
+
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(newUsers), (err, _) => {
+      return res.json({ status: "Success", id });
+    });
   })
   .delete((req, res) => {
-    //Delete user with ID
-    return res.json({ status: "Pending" });
+    const id = Number(req.params.id);
+    const newUsers = users.filter((user) => user.id !== id);
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(newUsers), (err, _) => {
+      return res.json({ status: "Success", id });
+    });
   });
 
 app.post("/api/users", (req, res) => {
   const body = req.body;
-  users.push({...body, id: users.length+1});
+  users.push({ ...body, id: users.length + 1 });
 
-  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err) => {
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, _) => {
     return res.json({ status: "Success", id: users.length });
   });
 });
