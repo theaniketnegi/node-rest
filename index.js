bc57a48ced88be0e27553170d5a3fe9b80c768c4
Aniket <aniketng08@gmail.com>
2023-11-30 23:54:00 +0530

    Added status codes

--- index.js
+++ index.js
@@ -22,12 +22,18 @@ app
   .route("/api/users/:id")
   .get((req, res) => {
     const id = Number(req.params.id);
-    return res.json(users.find((user) => user.id === id));
+    const user = users.find((user) => user.id === id)
+    if(!user)
+      return res.status(404).json({msg:"Not found!"});
+    return res.json(user);
   })
   .patch((req, res) => {
     const id = Number(req.params.id);
     const newUser = req.body;
 
+    const checkUserExists = users.find(user=>user.id===id) ? true : false;
+    if(!checkUserExists)
+      return res.status(404).json({msg:"User doesn't exist"});
     const newUsers = users.map((user) => {
       if (user.id === id) return { ...newUser, id };
       else return user;
@@ -47,10 +53,19 @@ app
 
 app.post("/api/users", (req, res) => {
   const body = req.body;
+  if (
+    !body ||
+    !body.first_name ||
+    !body.last_name ||
+    !body.gender ||
+    !body.email ||
+    !body.job_title
+  )
+    res.status(400).json({ msg: "All fields are required!" });
   users.push({ ...body, id: users.length + 1 });
 
   fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, _) => {
-    return res.json({ status: "Success", id: users.length });
+    return res.status(201).json({ status: "Success", id: users.length });
   });
 });
 
